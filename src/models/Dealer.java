3ad50abe10b03f51f900efd0e46653967ece47ca
methuliH <methuli.thinara@gmail.com>
2026-07-02 09:58:58 +0530

    Added getLocation method to Dealer Class

--- src/models/Dealer.java
+++ src/models/Dealer.java
@@ -33,4 +33,8 @@ public class Dealer {
     public void setLocation(String location) {
         this.location = location;
     }
+
+    public String getLocation() {
+        return location;
+    }
 }
